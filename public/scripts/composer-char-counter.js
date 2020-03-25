7c47dbdc35b07b1cbffaa43fc763bd1928cb2fe9
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-25 00:56:08 +0000

    added counter for tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(() => {
+  $("#tweet-text").on("keyup keydown", function() {
+    let tweetLength = $(this).val().length;
+    let remainder = $(this).siblings(".counter")[0];
+    remainder.value = 140 - tweetLength;
+
+    if (remainder.value < 0) {
+      $remainder.addClass("invalid");
+    } else {
+      $remainder.removeClass("invalid");
+    }
+  });
+});
